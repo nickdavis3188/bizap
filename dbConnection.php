d685290223394d9f0b388b551f4ab634d44cdbd1
nickdavis3188 <nyaknodavis318@gmail.com>
2022-05-07 18:57:33 +0100

    config

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,7 @@
     $conString = "mysql://b883bbc137e326:2bfc946e@us-cdbr-east-05.cleardb.net/heroku_e7f4dc073878fe6?reconnect=true";
     $host = "us-cdbr-east-05.cleardb.net";
     $user = "b883bbc137e326";
-    $db = "bizap";
+    $db = "heroku?e7f4dc073878fe6";
     $pwd = "2bfc946e";
     $conn = mysql_connect($host,$user,$pwd)or die("Problems :".mysql_error());
     $dbselect = mysql_select_db($db,$conn) or die("Error selecting DB: ".mysql_error());
